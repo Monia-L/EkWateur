337d0067ed6eb29ff9f914ff9578528ceeb0723d
Laoufi <monia@koji.fr>
2021-04-29 16:34:16 +0200

    fix(app): remove fragment on app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,5 @@ import React from "react";
 import GlobalRouter from "./routers/GlobalRouter";
 import "./styles/main.scss";
 
-const App = () => (
-  <>
-    <GlobalRouter />
-  </>
-);
+const App = () => <GlobalRouter />;
 export default App;
